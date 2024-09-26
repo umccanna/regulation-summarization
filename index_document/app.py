aac9287edae4cb441ed933dc6f50af9ed570b8a9
Uriah McCanna <Uriah.McCanna@milliman.com>
2024-09-26 13:02:01 -0400

    add conversation history to tool with embedding feedback look up checks

--- index_document/app.py
+++ index_document/app.py
@@ -3,10 +3,10 @@ from config import get_config
 import re
 from openai import AzureOpenAI
 from azure.cosmos import CosmosClient, ContainerProxy
-import psutil
-import os
 import getopt
 import sys
+from pathlib import Path 
+import json
 
 def normalize_text(text: str):
     text = re.sub(r'\s+',  ' ', text).strip()
@@ -27,10 +27,6 @@ def strip_emails_and_phone_numbers_and_web_addresses(text):
 
     return text
 
-def log_memory_usage():
-    process = psutil.Process(os.getpid())
-    print(f"Memory usage: {process.memory_info().rss / 1024 ** 2:.2f} MB")
-
 def generate_embeddings(client: AzureOpenAI, text: str):
     return client.embeddings.create(input = [text], model = get_config("EmbeddingsModel")).data[0].embedding
 
@@ -47,7 +43,7 @@ def create_document(cosmos_container: ContainerProxy, text: str, embeddings: lis
         "text": text,
         "vector": embeddings,
         "documentType": document_type,
-        "paritionKey": model_type
+        "partitionKey": model_type
     }
     cosmos_container.upsert_item(item)
 
@@ -55,7 +51,7 @@ def search_embeddings(embedding: list[float], model_type):
     container = get_cosmos_container()
     items = []
     for item in container.query_items(
-        query="SELECT TOP 20 c.id, c.modelType, c.text, c.pageIndex, c.documentType, VectorDistance(c.vector, @embedding) as similiarityScore FROM c ORDER BY VectorDistance(c.vector, @embedding)",
+        query="SELECT TOP 5 c.id, c.modelType, c.text, c.pageIndex, c.documentType, VectorDistance(c.vector, @embedding) as similiarityScore FROM c ORDER BY VectorDistance(c.vector, @embedding)",
         parameters=[dict(
             name="@embedding", value=embedding
         )],
@@ -95,11 +91,7 @@ def upload_fact_sheet():
             print(f"Saving page {i+1}")
             create_document(cosmos_container, normalized_page_text, embeddings, i, f"{model}_{year}", "FactSheet")
             
-
-def upload_final_ruling(): 
-    
-    #tracemalloc.start()
-
+def upload_final_ruling():
     print('Uploading Final Ruling')
 
     openai_client = AzureOpenAI(
@@ -197,6 +189,72 @@ def overlap_and_upload_chunks(cosmos_container, chunk_accumulator, overlap_size,
         print(f"Saving chunk {i+1+total_already_uploaded} of {total_chunks}")
         create_document(cosmos_container, chunked_data, embeddings, i+total_already_uploaded, f"{model}_{year}", "FinalRuling")
 
+def should_pull_more_embeddings(query: str):
+
+    historical_messages = get_history()
+    if len(historical_messages) == 0:
+        return True
+    
+    openai_client = AzureOpenAI(
+        api_key = get_config("AZURE_OPENAI_API_KEY"),
+        api_version = "2024-02-01",
+        azure_endpoint = get_config("AZURE_OPENAI_ENDPOINT")
+    )
+    
+    messages = []
+
+    messages.append({
+        "role": "system",
+        "content": "You are a CMS Regulation Analyst that analyzes pricing regulations and provides concise and accurate summaries of the regulations.  Your main job is to assist the user with summarizing and providing interesting insights into the regulations"
+    })
+
+    for message in historical_messages:
+        messages.append(message)
+
+    messages.append({
+                "role": "user",
+                "content": f'''
+                    Only provide a simple "yes" or "no" in response to this question.  Always respond in all lower case. 
+
+                    I have a vector database full of information regarding CMS regulations.  Based on the below "Prompt", do I need to provide additional context from this database to answer this question?
+                    Please be very careful in how you respond and verify that the answer is the correct answer based on the conversaion history.
+
+                    Prompt:
+                    {query}
+                ''',
+            })  
+
+    chat_completion = openai_client.chat.completions.create(
+        messages=messages,
+        model=get_config("ChatModel")
+    )
+
+    decision = chat_completion.choices[0].message.content
+    
+    print(f'Initial embeddings question response "{decision}"')
+
+    messages.append({
+        "role": "assistant",
+        "content": decision
+    })
+
+    messages.append({
+        "role": "user",
+        "content": '''Could you double check your previous response and verify that it was correct? If it was correct, echo the exact response.  Otherwise provide the correct response. Only response with a "yes" or "no".
+
+            You Previous Answer: {decision}'''
+    })
+
+    chat_completion = openai_client.chat.completions.create(
+        messages=messages,
+        model=get_config("ChatModel")
+    )
+    
+    decision = chat_completion.choices[0].message.content
+
+    print(f'Checked embeddings question response "{decision}"')
+
+    return decision.lower().strip() == 'yes'
 
 def query_embeddings(query: str):
     openai_client = AzureOpenAI(
@@ -224,7 +282,29 @@ def query_embeddings(query: str):
     
     return results
 
-def prompt_open_ai(embeddings: list[str], query: str):
+def get_history():
+    conversation_history = []
+    cache_path = Path('./conversation_history/convo_cache.json')
+    if not cache_path.exists():
+        return conversation_history
+    
+    with open(cache_path, 'r') as convo_reader:
+        cached_conversations = json.load(convo_reader)
+        for c in cached_conversations:
+            conversation_history.append(c)
+    
+    return conversation_history
+        
+def add_to_history(new_convo):
+    existing_history = get_history()
+    for c in new_convo:
+        existing_history.append(c)
+    cache_path = Path('./conversation_history/convo_cache.json')
+    with open(cache_path, 'w') as convo_writer:
+        history_json = json.dumps(existing_history)
+        convo_writer.write(history_json)
+
+def prompt_open_ai_with_embeddings(embeddings: list[str], query: str):
     openai_client = AzureOpenAI(
         api_key = get_config("AZURE_OPENAI_API_KEY"),
         api_version = "2024-02-01",
@@ -233,13 +313,23 @@ def prompt_open_ai(embeddings: list[str], query: str):
 
     context = " ".join(embeddings)
 
-    chat_completion = openai_client.chat.completions.create(
-        messages=[
-            {
+    historical_messages = get_history()
+    
+    messages = []
+
+    messages.append({
+        "role": "system",
+        "content": "You are a CMS Regulation Analyst that analyzes pricing regulations and provides concise and accurate summaries of the regulations.  Your main job is to assist the user with summarizing and providing interesting insights into the regulations"
+    })
+
+    for message in historical_messages:
+        messages.append(message)
+
+    new_user_message = {
                 "role": "user",
                 "content": f'''
-                    You are a CMS Regulation Analyst that analyzes pricing regulations and provides concise and accurate responses.  
-                    Only provide responses that can be found in the provided "Context:" based on the user's "Prompt:".  If you are unable to find a response in the provided context do not make anything up.  Just response with "I'm not sure".
+                    Only provide responses that can be found in the below "Context:" or from "Context:" in their previous messages based on the user's "Prompt:".  
+                    If you are unable to find a response in the below context or previous "Context:" do not make anything up.  Just response with "I'm not sure".
 
                     Prompt:
                     {query}
@@ -248,15 +338,68 @@ def prompt_open_ai(embeddings: list[str], query: str):
                     {context}
                 ''',
             }
-        ],
+
+    messages.append(new_user_message)    
+
+    chat_completion = openai_client.chat.completions.create(
+        messages=messages,
         model=get_config("ChatModel")
     )
 
     print(chat_completion.choices[0].message.content)
 
+    add_to_history([new_user_message, {
+        "role": "assistant",
+        "content": chat_completion.choices[0].message.content
+    }])
+
+def prompt_open_ai(query: str):
+    openai_client = AzureOpenAI(
+        api_key = get_config("AZURE_OPENAI_API_KEY"),
+        api_version = "2024-02-01",
+        azure_endpoint = get_config("AZURE_OPENAI_ENDPOINT")
+    )
+
+    historical_messages = get_history()
+    
+    messages = []
+
+    messages.append({
+        "role": "system",
+        "content": '''You are a CMS Regulation Analyst that analyzes pricing regulations and provides concise and accurate summaries of the regulations.  
+            Your main job is to assist the user with summarizing and providing interesting insights into the regulations'''
+    })
+
+    for message in historical_messages:
+        messages.append(message)
+
+    new_user_message = {
+                "role": "user",
+                "content": query,
+            }
+
+    messages.append(new_user_message)    
+
+    chat_completion = openai_client.chat.completions.create(
+        messages=messages,
+        model=get_config("ChatModel")
+    )
+
+    print(chat_completion.choices[0].message.content)
+
+    add_to_history([new_user_message, {
+        "role": "assistant",
+        "content": chat_completion.choices[0].message.content
+    }])
+
+def execute_clear_history(): 
+    cache_path = Path('./conversation_history/convo_cache.json')
+    if cache_path.exists():
+        cache_path.unlink()
+
 def main():
     try:
-        opts, _ = getopt.getopt(sys.argv[1:], "q:", ["query="])
+        opts, _ = getopt.getopt(sys.argv[1:], "q:crf", ["query=", "clear-history", 'upload-final-ruling', 'upload-fact-sheet'])
     except getopt.GetoptError:
         print_help()
         sys.exit(2)
@@ -265,18 +408,33 @@ def main():
         print_help()
         sys.exit(0)
 
-    query = get_value(opts, '-q')
-    if query == None:
-        print('No query provided.  Please provide a query to begin')
-        print_help()
-        sys.exit(2)
+    clear_history = get_flag(opts, '-c')
+    if clear_history:
+        print('Clearing convo history...')
+        execute_clear_history()
 
-    embeddings = query_embeddings(query)
-    if len(embeddings) == 0:
-        print('We are unable to complete your request at this time')
-        sys.exit(0)
+    query = get_value(opts, '-q')
+    if query != None:
+        should_get_embeddings = should_pull_more_embeddings(query)
+        if should_get_embeddings:
+            embeddings = query_embeddings(query)
+            if len(embeddings) == 0:
+                print('We are unable to complete your request at this time. We were unable to find context for your query.')
+                sys.exit(0)
+            prompt_open_ai_with_embeddings([e["text"] for e in embeddings], query)
+        else:
+            prompt_open_ai(query)
+        
     
-    prompt_open_ai([e["text"] for e in embeddings], query)
+    upload_ruling = get_flag(opts, '-r')
+    if upload_ruling:
+        upload_final_ruling()
+
+    upload_fact = get_flag(opts, '-f')
+    if upload_fact:
+        upload_fact_sheet()
+
+    
 
 
 def requesting_help(opts):
@@ -289,6 +447,10 @@ def print_help():
 def get_value(opts, parameter):
     value_arg = next((o for o in opts if len(o) > 0 and o[0] == parameter and len(o) == 2), None)
     return value_arg[1] if value_arg != None else None
+
+def get_flag(opts, parameter):
+    value_arg = next((o for o in opts if len(o) > 0 and o[0] == parameter), None)
+    return value_arg != None
 
 if __name__ == "__main__":
 	sys.exit(main())
